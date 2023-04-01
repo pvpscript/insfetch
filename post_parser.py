371ba650b4046d633bd650be28b085c700aa2794
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2023-04-01 01:35:24 -0300

    feat: adding post html parser

---
+++ post_parser.py
@@ -0,0 +1,24 @@
+from html.parser import HTMLParser
+
+class PostParser(HTMLParser):
+    _data = None
+    _flag_found_data = False
+    _flag_disable = False
+
+    def _match_attrs(self, attrs):
+        return [a[0] == 'type' and a[1] == 'application/ld+json'
+                for a in attrs]
+
+    def handle_starttag(self, tag, attrs):
+        if (self._flag_found_data == False and
+                tag == 'script' and self._match_attrs(attrs)):
+            self._flag_found_data = True
+
+    def handle_data(self, data):
+        if self._flag_found_data and self._flag_disable == False:
+            self._data = data
+            self._flag_disable = True
+
+    @property
+    def data(self):
+        return self._data
