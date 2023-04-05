47ad5107136efe73b59cb15b92c09e395bdd609c
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2023-04-04 22:16:16 -0300

    feat: adding FilteredParams decorator

---
+++ insfetch/utils/filtered_params.py
@@ -0,0 +1,17 @@
+from typing import List, Dict, Any
+
+class FilteredParams:
+    def __init__(self, cls):
+        self._cls = cls
+
+    def __call__(self, *args, **kwargs):
+        keys: List[str] = self._cls.__keys__
+        ref_dict: Dict[str, Any] = kwargs.pop('__ref__', None)
+
+        instance = self._cls(*args, **kwargs)
+
+        if keys is not None and ref_dict is not None:
+            for k in keys:
+                setattr(instance, k, ref_dict[k])
+
+        return instance
