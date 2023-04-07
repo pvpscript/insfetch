4217247dbbef076584780c7ec4cff7cf7d8f1a65
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2023-04-07 03:15:44 -0300

    chore: using autoreffering attributes

--- insfetch/post/post_handler.py
+++ insfetch/post/post_handler.py
@@ -3,10 +3,13 @@ import json
 
 from insfetch.post.content.image import Image
 
+from insfetch.utils.autorrefering_attributes import AutorefferingAttributes
 from insfetch.utils.funcs import get_proxies, cc_get
 
+@AutorefferingAttributes
 class PostHandler:
-    _post_dict = None
+    __attributes__ = ['articleBody', 'commentCount', 'contentLocation',
+                      'dateCreated', 'dateModified', 'headline']
 
     def __init__(self, post_id, post_page_parser):
         self._post_id = post_id
@@ -27,65 +30,25 @@ class PostHandler:
         self._post_page_parser.feed(data.text)
         return json.loads(self._post_page_parser.data)
 
-    # TODO: code below needs refactoring!
+    def _match_interaction(self, interaction):
+        parsed_type = interaction['interactionType'].split('/')[-1]
+        count = interaction['userInteractionCount']
 
-    def article_body(self): # [root]
-        return cc_get(self._post_dict, ['articleBody'])
+        match parsed_type:
+            case 'CommentAction': return {'comments': count}
+            case 'LikeAction': return {'likes': count}
+            case 'WatchAction': return {'views': count}
+            case _: return {'unmapped': count}
+
+    def identifier(self):
+        return cc_get(self._post_dict, ['identifier', 'value'])
+
+    def interaction_statistic(self):
+        interactions = [self._match_interaction(i)
+                        for i in cc_get(self._post_dict, ['interactionStatistic'])]
+
+        return {k: v for i in interactions for k, v in i.items()}
 
     @property
     def image(self):
         return Image(self._post_dict['image'])
-
-    def num_videos(self): # video
-        pass
-
-    def video_dimensions(self, index): # video
-        pass
-
-    def video_url(self, index): # video
-        pass
-
-    def video_thumbnail(self, index): # video
-        pass
-
-    def video_upload_date(self, index): # video
-        pass
-
-    def video_likes(self, index): # video
-        pass
-
-    def author_username(self): # author
-        return cc_get(self._post_dict, ['author', 'identifier', 'value'])
-
-    def author_name(self): # author
-        return cc_get(self._post_dict, ['author', 'name'])
-
-    def author_image(self): # author
-        return cc_get(self._post_dict, ['author', 'image'])
-
-    def num_comments(self): # interactionStatistic
-        pass
-
-    def num_likes(self): # interactionStatistic
-        pass
-
-    def top_comment(self): # comment
-        pass
-
-    def top_comment_author_name(self): # comment
-        pass
-
-    def top_comment_author_username(self): # comment
-        pass
-
-    def top_comment_author_image(self): # comment
-        pass
-
-    def content_location(self): # [root]
-        pass
-
-    def creation_date(self): # [root]
-        pass
-
-    def modification_date(self): # [root]
-        pass
