3a0769aa90d68f13e8d24f1851067d82e064e56a
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2023-04-08 02:11:37 -0300

    feat: support for dict attributes

--- insfetch/utils/autoreffering_attributes.py
+++ insfetch/utils/autoreffering_attributes.py
@@ -4,15 +4,26 @@ class AutorefferingAttributes:
     def __init__(self, cls):
         self._cls = cls
 
+    def _split_dict(self, ref_dict):
+        return next((k, v) for k, v in ref_dict.items())
+
     def __call__(self, *args, **kwargs):
-        keys: List[str] = self._cls.__attributes__
+        attributes: List[str] = self._cls.__attributes__
+        dict_attributes: List[dict] = self._cls.__dict_attributes__
         ref_dict: Dict[str, Any] = kwargs.pop('__ref__', None)
 
         instance = self._cls(*args, **kwargs)
 
-        if keys is not None and ref_dict is not None:
-            for k in keys:
-                if k in ref_dict.keys():
-                    setattr(instance, k, ref_dict[k])
+        if attributes is not None and ref_dict is not None:
+            for attr in attributes:
+                if attr in ref_dict.keys():
+                    setattr(instance, attr, ref_dict[attr])
+
+        if dict_attributes is not None and ref_dict is not None:
+            for attr in dict_attributes:
+                orig_attr, new_attr = self._split_dict(attr)
+
+                if orig_attr in ref_dict.keys():
+                    setattr(instance, new_attr, ref_dict[orig_attr])
 
         return instance
