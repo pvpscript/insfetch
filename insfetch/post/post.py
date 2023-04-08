68690db4a21cce7260a3965fa31a027632f30b22
Pedro Vitor Piassa <33137099+pvpscript@users.noreply.github.com>
2023-04-08 02:13:03 -0300

    chore: separating post from post handler

---
+++ insfetch/post/post.py
@@ -0,0 +1,61 @@
+from insfetch.post.content.image import Image
+from insfetch.post.content.video import Video
+from insfetch.post.content.author import Author
+from insfetch.post.content.comment import Comment
+
+from insfetch.utils.autoreffering_attributes import AutorefferingAttributes
+from insfetch.utils.funcs import cc_get
+
+@AutorefferingAttributes
+class Post:
+    __attributes__ = ['headline']
+    __dict_attributes__ = [{'articleBody': 'article_body'},
+                           {'commentCount': 'comment_count'},
+                           {'contentLocation': 'content_location'},
+                           {'dateCreated': 'date_created'},
+                           {'dateModified': 'date_modified'}]
+
+    def __init__(self, data):
+        self._data = data
+
+    def _match_interaction(self, interaction):
+        parsed_type = interaction['interactionType'].split('/')[-1]
+        count = interaction['userInteractionCount']
+
+        match parsed_type:
+            case 'CommentAction':
+                return {'comments': count}
+            case 'LikeAction':
+                return {'likes': count}
+            case 'WatchAction':
+                return {'views': count}
+            case _:
+                return {'unmapped': count}
+
+    def identifier(self):
+        return cc_get(self._data, ['identifier', 'value'])
+
+    def interaction_statistic(self):
+        interactions = [self._match_interaction(i)
+                        for i in cc_get(self._data, ['interactionStatistic'])]
+
+        return {k: v for i in interactions for k, v in i.items()}
+
+    def author(self):
+        if (author_dict := self._data.get('author')) is not None:
+            return Author(author_dict, __ref__=author_dict)
+
+        return None
+
+    def comment(self):
+        if (comment_dict := self._data.get('comment')) is not None:
+            return Comment(comment_dict, __ref__=comment_dict)
+
+        return None
+
+    @property
+    def images(self):
+        return [Image(__ref__=i) for i in self._data.get('image')]
+
+    def videos(self):
+        return [Video(__ref__=v) for v in self._data.get('video')]
--- insfetch/post/post_handler.py
+++ insfetch/post/post_handler.py
@@ -1,19 +1,9 @@
 import requests
 import json
 
-from insfetch.post.content.image import Image
-from insfetch.post.content.video import Video
-from insfetch.post.content.author import Author
-from insfetch.post.content.comment import Comment
+from insfetch.utils.funcs import get_proxies
 
-from insfetch.utils.autorrefering_attributes import AutorefferingAttributes
-from insfetch.utils.funcs import get_proxies, cc_get
-
-@AutorefferingAttributes
 class PostHandler:
-    __attributes__ = ['articleBody', 'commentCount', 'contentLocation',
-                      'dateCreated', 'dateModified', 'headline']
-
     def __init__(self, post_id, post_page_parser):
         self._post_id = post_id
         self._post_page_parser = post_page_parser
@@ -33,34 +23,6 @@ class PostHandler:
         self._post_page_parser.feed(data.text)
         return json.loads(self._post_page_parser.data)
 
-    def _match_interaction(self, interaction):
-        parsed_type = interaction['interactionType'].split('/')[-1]
-        count = interaction['userInteractionCount']
-
-        match parsed_type:
-            case 'CommentAction': return {'comments': count}
-            case 'LikeAction': return {'likes': count}
-            case 'WatchAction': return {'views': count}
-            case _: return {'unmapped': count}
-
-    def identifier(self):
-        return cc_get(self._post_dict, ['identifier', 'value'])
-
-    def interaction_statistic(self):
-        interactions = [self._match_interaction(i)
-                        for i in cc_get(self._post_dict, ['interactionStatistic'])]
-
-        return {k: v for i in interactions for k, v in i.items()}
-
-    def author(self):
-        return [Author(a, __ref__=a) for a in self._post_dict.get('author')]
-
-    def comment(self):
-        return [Comment(c, __ref__=c) for c in self._post_dict.get('comment')]
-
     @property
-    def image(self):
-        return [Image(__ref__=i) for i in self._post_dict.get('image')]
-
-    def video(self):
-        return [Video(__ref__=v) for v in self._post_dict.get('video')]
+    def post_dict(self):
+        return self._post_dict
